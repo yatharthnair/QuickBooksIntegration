6ec6d7628a668535e853c6d734943ec8e955401f
Dr. Servleen Kaur <yatharthnair04@gmail.com>
2023-03-07 20:46:18 +0530

    View Vendor

--- QuickbookIntegration1/Controllers/HomeController.cs
+++ QuickbookIntegration1/Controllers/HomeController.cs
@@ -6,6 +6,14 @@ using System.Diagnostics;
 using System.Security.Claims;
 using Microsoft.EntityFrameworkCore;
 using QuickbookIntegration1.Models;
+using Intuit.Ipp.Data;
+using Intuit.Ipp.DataService;
+using Intuit.Ipp.OAuth2PlatformClient;
+using Intuit.Ipp.Security;
+using NuGet.Common;
+using System;
+using Intuit.Ipp.Core;
+using static System.Net.WebRequestMethods;
 
 namespace IntegrationWithQuickbooks.Controllers
 {
@@ -61,15 +69,26 @@ namespace IntegrationWithQuickbooks.Controllers
         {
             return View();
         }
-        public IActionResult AddVendors()
-        {
-            return View();
-        }
 
+        /*public IActionResult ViewVendor()
+        {   
+            return View();
+        }*/
+
+
+
+        [HttpGet]
         public IActionResult ViewVendor()
         {
-            return View();
+            var data = new List<VendorList>();
+            using (var context = new QuickBooksContext())
+            {
+                data = context.VendorLists.ToList();
+            }
+
+            return View(data);
         }
+
 
         [HttpPost]
         public ActionResult AddVendor(VendorList model)
@@ -80,6 +99,25 @@ namespace IntegrationWithQuickbooks.Controllers
                 context.SaveChanges();
             }
             return View();
+        }
+        public IActionResult OAuth2Client()
+        {
+            string clientId = "ABmvAWjedrPeDs7xZZm7OmVtskZ7yxg4S3wvlKq2sbOzClLvf9";
+            string clientSecret = "vVxCwSHNZXitBtNjYfDbcGZaDJxY33M6vJpdrZBm";
+            string redirectUrl = "https://localhost:7092/Home/ViewVendor";
+            string environment = "sandbox";
+ 
+            OAuth2Client oauthClient = new OAuth2Client(clientId, clientSecret, redirectUrl, environment);
+
+
+            List<OidcScopes>? scopes = null;
+            string authUrl = oauthClient.GetAuthorizationURL(scopes);
+
+            string authCode = Request.Query["code"];
+            var token = oauthClient.GetBearerTokenAsync(authCode);
+            OAuth2RequestValidator oauthValidator = new OAuth2RequestValidator("eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..kzfe1IkaGrWQSUzKYfa4LQ.J2cnU-l_yAeuko85plp0BdHS4SOXtErB7pAkGuvqIBHoI8iHgOyAf4vRVfaCut55gOnM1BZMNAifRwzEWKxrwDXeQ9ZyM2KZDo0sb4FniAiSOMMCQ30MKTBMtvu1I1NrBVc7Kskk41w2-E69rIKlWjwC5t8QH5I4GaO5I-dV0M82pD46TnXbfFUVPqF7VVAVcNsgbYFGa5T39XmguLbTaZiDVdv_ghOAfHNG_kWZTqfT0BRoUts5QZr1qRfeqw2pn-EQUwkJmZWYJXUsN9M9bUT1xDkUly_XqAHbzSXcKz8y1PSfftCnf2c-YqqtDEVoQtivx1X07SWPsPxKoioZ9WwbkeEQ2yRsFGXnxOPSSSA4E52UvWaxu1cc3jnV8nui9aV6Ueqb7P89nE7riTKor_npIhGkG8yldl1g63f-ka6cr3_XLbmHyFB-wxZvPiW9A8dEAlkWh8J0emabMkQMvTMjvlO35mIyITIPlFcVT1X5BIDHC1KHCAFA6PJordDIT_beeOn3Oqmw9nrQKuq2UHQhT-cYQogB5lwIckMmilLZc1gghTL3zo600knSxRwB8a9NW6AGoeNH89t8qEUGFS3OCAR16YI8eAuNvg2dSHx8mGBMKTYzMLYSqHpr5y5Q5tKpKDCIfFBrytTU0ITH2byBUD6KVa2Vjmm8E9hxHfJSGT7bjy1JPxeXvu--Rtq_mehh7J5MGTBTYB6jY6d0cqzXjwz2WjNfeLaT_fgHUC-Q5SLUmmVq8ACKOcHae1G9.b3q8DODnQRQflzOB8sZqRw");
+            DataService service = new DataService(oauthValidator);
+
         }
     }
 }
--- QuickbookIntegration1/Program.cs
+++ QuickbookIntegration1/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
